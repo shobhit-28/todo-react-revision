577e45d4fcb39ca2ae1b0d0f160ef0da4a825628
Shobhit Raj <Shobhit.Raj@ril.com>
2026-02-05 19:50:51 +0530

    added add btn

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import { NavBar } from './components/navBar/navBar'
 import { HomePage } from './components/homePage/homePage'
 import { Calendar } from './components/calendar/calendar'
+import { AddBtn } from './components/addNewTodo/addBtn/addBtn'
 
 function App() {
 
   return (
     <>
       <NavBar />
+      <AddBtn />
       <Calendar />
       <Routes>
         <Route path='/' element={<HomePage />} />
---
+++ src/components/addNewTodo/addBtn/addBtn.jsx
@@ -0,0 +1,7 @@
+import { IoMdAdd } from "react-icons/io"
+
+export const AddBtn = () => {
+    return (
+        <button className="fixed block bottom-4 right-4 text-4xl bg-teal-950/90 hover:bg-teal-950 hover:text-teal-100 text-teal-100/80 rounded-full p-2"><IoMdAdd /></button>
+    )
+}
